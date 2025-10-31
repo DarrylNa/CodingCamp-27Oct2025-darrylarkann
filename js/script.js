004a94c4d54ff35789172fe5a5c64e0451d23af6
darrylnalanda <107294779+darrylnalanda@users.noreply.github.com>
2025-10-31 23:17:39 +0700

    membuat array ksong untuk daftar tugas dan mengambil elemen input form di js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let todos = [];
+let currentFilter = 'all';
+
+const todoForm = document.getElementById('todoForm');
+const todoInput = document.getElementById('todoInput');
+const dateInput = document.getElementById('dateInput');
+const todoList = document.getElementById('todoList');
+const emptyState = document.getElementById('emptyState');
+const filterButtons = document.querySelectorAll('.btn-filter');
+
+
